0ea1590355f62de6bcdbfd284af87b819f31bc04
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-13 21:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const fname = document.querySelectorAll('input[name=fname]')[0].value;
+	const lname = document.querySelectorAll('input[name=lname]')[0].value;
+	alert(`${fname} ${lname}`);
 }
